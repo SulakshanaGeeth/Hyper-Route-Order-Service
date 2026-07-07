75bd5a7d3d9da7cada5cac0cb6c5d3a134b355f2
Geeth <sulakshanageeth@gmail.com>
2026-07-07 22:42:35 +0530

    initalized project

---
+++ src/main/java/com/hyperroute/hyperroute/HyperrouteApplication.java
@@ -0,0 +1,13 @@
+package com.hyperroute.hyperroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperrouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyperrouteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyperroute/hyperroute/HyperrouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyperroute.hyperroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperrouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
